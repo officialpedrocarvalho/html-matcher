98e28820f960378925fc0195dee6dec4ba5abc88
officialpedrocarvalho <pedrocarvalho@gmail.com>
2022-04-08 17:09:56 +0100

    Fix some bugs

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 
-VERSION = '0.0.7'
+VERSION = '0.0.8'
 DESCRIPTION = 'Algorithms to find similarity between HTML pages.'
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
